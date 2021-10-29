f88cb5dfa071a677d8387883c0d5c7da244f36d0
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-10-29 10:06:34 -0400

    Update sequence.js

--- js/sequence.js
+++ js/sequence.js
@@ -1,7 +1,9 @@
+var setVal = null
 function calcSequenceTerm(t,d,ft){
     return ft+d*(t-1)
 }
 function sequenceMain(){
-    const tb = String.split(document.getElementById("textInput1").value, ",")
-    const 
+    var str = document.getElementById("textInput1").value
+    const tb = str.split(",")
+    setVal = str
 }
